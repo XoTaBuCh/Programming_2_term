3992fbf67d3bc5deca19c699eb3ca6ce30dc6cee
MAX <58146871+XoTaBuCh@users.noreply.github.com>
2021-03-10 14:53:32 +0300

    Update main_test.c

--- 1/main_test.c
+++ 1/main_test.c
@@ -3,18 +3,18 @@
 #include <assert.h>
 
 int test_double() {
-assert(Double("123.456") == 15);
-assert(Double("123.111") == 6);
-assert(Double("0.0") == 0);
-assert(Double("1") == 1);
-assert(Double("457") == 16);
-assert(Double("0.784") == 19);
-assert(Double("99999999999.9999") == 99);
-assert(Double("0") == 0);
-assert(Double("11111.111111111111") == 12);
-assert(Double("sfhdzfhdz") == -1);
-}
+    assert(Double("123.456") == 15);
+    assert(Double("123.111") == 6);
+    assert(Double("0.0") == 0);
+    assert(Double("1") == 1);
+    assert(Double("457") == 16);
+    assert(Double("0.784") == 19);
+    assert(Double("99999999999.9999") == 99);
+    assert(Double("0") == 0);
+    assert(Double("11111.111111111111") == 12);
+    assert(Double("sfhdzfhdz") == -1);
+    }
 #undef main
 int main() {
-test_double();
+    test_double();
 }
