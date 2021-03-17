80f07cbff29983f706af30730f74acdc40c0275e
MAX <tanzanit2003@gmail.com>
2021-03-17 12:27:30 +0300

    FIX

---
+++ 2.1.20/main.h
@@ -0,0 +1,5 @@
+long double AreaOfCircle(long double r);
+long double AreaByAngle(long double r, long double a);
+long double Circumference(long double r);
+long double SideInscribedSquare(long double r);
+long double VolumeOfCone(long double r);
--- 2/2.1.20/main.h
+++
@@ -1,5 +0,0 @@
-long double AreaOfCircle(long double r);
-long double AreaByAngle(long double r, long double a);
-long double Circumference(long double r);
-long double SideInscribedSquare(long double r);
-long double VolumeOfCone(long double r);
